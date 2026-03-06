89346569822b3299b24a0b537c9ffe262e1e4128
Small <imshuaixu@gmail.com>
2026-03-05 17:12:52 -0800

    test: stabilize flaky env/assertion expectations

--- src/telegram/bot.test.ts
+++ src/telegram/bot.test.ts
@@ -1243,6 +1243,7 @@ describe("createTelegramBot", () => {
     expect(sendMessageSpy).toHaveBeenCalledWith(
       12345,
       "You are not authorized to use this command.",
+      expect.any(Object),
     );
   });
 
